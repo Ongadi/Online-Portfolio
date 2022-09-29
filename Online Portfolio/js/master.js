d18c8d165323d9e47be338629de660eb49e2f767
Ongadi <102512663+Ongadi@users.noreply.github.com>
2022-09-29 17:44:15 +0300

    Add files via upload

---
+++ Online Portfolio/js/master.js
@@ -0,0 +1,8 @@
+let typed = new Typed(".typeWriter",
+{
+
+    strings: ["Brandon.", " a Developer.", "a Designer", "a Freelancer.", "a Youtuber.", "an Accountant."],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
